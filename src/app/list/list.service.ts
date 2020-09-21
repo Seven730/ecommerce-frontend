b657058f6cba584e5a4b1397487b085de4a80a14
Seven <michalwww3@gmail.com>
2020-09-21 10:15:19 +0200

    Added Service Component

---
+++ src/app/list/list.service.ts
@@ -0,0 +1,8 @@
+import { stringify } from 'querystring';
+
+export class ListService {
+  url: string = 'http://127.0.0.1:8000/api/book/?format=json';
+  getList() {
+    // Space for HTTPS request
+  }
+}
